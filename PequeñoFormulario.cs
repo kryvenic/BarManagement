b7b09294ae7430f648ea03c89f526a7202a4c284
Nicolas <nicolaskry@hotmail.com>
2021-08-13 10:50:57 -0300

    Agregado Proyecto TP2y3

---
+++ PequeñoFormulario.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Practico3
+{
+    public partial class PequeñoFormulario : Form
+    {
+        public PequeñoFormulario()
+        {
+            InitializeComponent();
+        }
+    }
+}
