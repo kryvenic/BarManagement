c4ab99013b54c65087608f3b5c92a4ed98c973d1
Nicolas <nicolaskry@hotmail.com>
2021-08-25 16:29:24 -0300

    Comenzando el Practico 3

--- Practicos/Practico3/Program.cs
+++ Practicos/Practico3/Program.cs
@@ -16,7 +16,7 @@ namespace Practico3
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new PequenioFormulario());
         }
     }
 }
