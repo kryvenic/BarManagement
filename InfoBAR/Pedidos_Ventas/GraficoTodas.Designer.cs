8773e4d32171df2d3c773e5cfe46807c717af46c
martincou29 <martincoutinho00@gmail.com>
2021-11-29 01:15:14 -0300

    Grafico todas las ventas, ahora con tipo de pago en la fecha

--- InfoBAR/Pedidos_Ventas/GraficoTodas.Designer.cs
+++ InfoBAR/Pedidos_Ventas/GraficoTodas.Designer.cs
@@ -29,10 +29,11 @@ namespace InfoBAR
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.btnGrafico = new System.Windows.Forms.Button();
+            this.btnMostrartipo = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -41,10 +42,10 @@ namespace InfoBAR
             this.chart1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            chartArea1.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea1);
-            legend1.Name = "Legend1";
-            this.chart1.Legends.Add(legend1);
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
             this.chart1.Location = new System.Drawing.Point(12, 54);
             this.chart1.Name = "chart1";
             this.chart1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
@@ -59,7 +60,7 @@ namespace InfoBAR
             this.btnGrafico.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnGrafico.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnGrafico.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(246)))), ((int)(((byte)(246)))), ((int)(((byte)(246)))));
-            this.btnGrafico.Location = new System.Drawing.Point(426, 12);
+            this.btnGrafico.Location = new System.Drawing.Point(12, 12);
             this.btnGrafico.Name = "btnGrafico";
             this.btnGrafico.Size = new System.Drawing.Size(295, 36);
             this.btnGrafico.TabIndex = 28;
@@ -67,11 +68,27 @@ namespace InfoBAR
             this.btnGrafico.UseVisualStyleBackColor = false;
             this.btnGrafico.Click += new System.EventHandler(this.btnGrafico_Click);
             // 
+            // btnMostrartipo
+            // 
+            this.btnMostrartipo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(92)))), ((int)(((byte)(92)))), ((int)(((byte)(167)))));
+            this.btnMostrartipo.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnMostrartipo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMostrartipo.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMostrartipo.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(246)))), ((int)(((byte)(246)))), ((int)(((byte)(246)))));
+            this.btnMostrartipo.Location = new System.Drawing.Point(313, 12);
+            this.btnMostrartipo.Name = "btnMostrartipo";
+            this.btnMostrartipo.Size = new System.Drawing.Size(295, 36);
+            this.btnMostrartipo.TabIndex = 29;
+            this.btnMostrartipo.Text = "Mostrar Con Tipo De Pago";
+            this.btnMostrartipo.UseVisualStyleBackColor = false;
+            this.btnMostrartipo.Click += new System.EventHandler(this.btnMostrartipo_Click);
+            // 
             // GraficoTodas
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1161, 546);
+            this.Controls.Add(this.btnMostrartipo);
             this.Controls.Add(this.btnGrafico);
             this.Controls.Add(this.chart1);
             this.Name = "GraficoTodas";
@@ -86,5 +103,6 @@ namespace InfoBAR
 
         private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
         private System.Windows.Forms.Button btnGrafico;
+        private System.Windows.Forms.Button btnMostrartipo;
     }
 }
--- InfoBAR/Pedidos_Ventas/GraficoTodas.cs
+++ InfoBAR/Pedidos_Ventas/GraficoTodas.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.Entity;
 using System.Data.Entity.Core.Objects;
 using System.Drawing;
 using System.Linq;
@@ -26,52 +27,128 @@ namespace InfoBAR
 
         private void btnGrafico_Click(object sender, EventArgs e)
         {
-
-            using (InfobarEntities db = new InfobarEntities())
+            try
             {
-                //Traer todas las ventas/pedidos con tipo de pago y usuario
-                var pedidosYDetalles = from pedi in db.Pedido
-                                       group pedi by EntityFunctions.TruncateTime(pedi.Fecha.Value) into pdf
-                                       select new
-                                       {
-                                           Fecha = EntityFunctions.TruncateTime(pdf.Key.Value),
-                                           Suma = pdf.Sum(pedi => pedi.Importe_Total).ToString(),
-                                       };
-                var tiposPagos = from tipo in db.TipoPago
-                                 select tipo.Descripcion;
-               
-                //Verificar si no se encontraron pedidos
-                if (pedidosYDetalles.Any() && tiposPagos.Any())
+                using (InfobarEntities db = new InfobarEntities())
                 {
-                    chart1.Series.Clear();
-                    chart1.Series.Add("Todas las ventas");
-                    //Añadir al chart
-                    foreach (var i in pedidosYDetalles)
-                    {
-                        string fecha = i.Fecha.Value.ToString("dd/MM/yyyy");
-                        
-                        try
-                        {
-                            chart1.Series[0].Points.AddXY(i.Fecha.Value.Date, double.Parse(i.Suma));
-                            chart1.Series[0].SmartLabelStyle.Enabled = true;
-                            chart1.Series[0].IsValueShownAsLabel = true;
+                    //Traer todas las ventas/pedidos con tipo de pago
+                    var pedidosYDetalles = from pedi in db.Pedido
+                                           where pedi.Id_TipoPago != null
+                                           group pedi by DbFunctions.TruncateTime(pedi.Fecha.Value) into pdf
+                                           select new
+                                           {
+                                               Fecha = DbFunctions.TruncateTime(pdf.Key.Value),
+                                               Suma = pdf.Sum(pedi => pedi.Importe_Total).ToString(),
+                                           };
 
-                        } catch (System.ArgumentException)
+
+                    //Verificar si no se encontraron pedidos
+                    if (pedidosYDetalles.Any())
+                    {
+                        chart1.Series.Clear();
+                        chart1.Series.Add("Todas las ventas");
+                        //Añadir al chart
+                        foreach (var i in pedidosYDetalles)
                         {
-                            //Si ya existe, referenciarla
-                            chart1.Series[0].Points.AddXY(i.Fecha.Value.Date, double.Parse(i.Suma));
+                            string fecha = i.Fecha.Value.ToString("dd/MM/yyyy");
+
+                            try
+                            {
+                                chart1.Series[0].Points.AddXY(i.Fecha.Value.Date, double.Parse(i.Suma));
+                                chart1.Series[0].SmartLabelStyle.Enabled = true;
+                                chart1.Series[0].IsValueShownAsLabel = true;
+                                chart1.Series[0]["PixelPointWidth"] = "50";
+
+                            }
+                            catch (System.ArgumentException)
+                            {
+                                //Si ya existe, referenciarla
+                                chart1.Series[0].Points.AddXY(i.Fecha.Value.Date, double.Parse(i.Suma));
+                                chart1.Series[0]["PixelPointWidth"] = "50";
+
+                            }
+
                         }
-                        
+
+                    }
+                    else
+                    {
+                        MessageBox.Show("No se encontraron pedidos", "Error", MessageBoxButtons.OK, MessageBoxIcon.Information);
                     }
 
                 }
-                else
-                {
-                    MessageBox.Show("No se encontraron pedidos", "Error", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                }
-
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("No se pudo traer los pedidos de la base de datos", "Error", MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
 
+
+        }
+
+        private void btnMostrartipo_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (InfobarEntities db = new InfobarEntities())
+                {
+                    //Traer todas las ventas/pedidos con tipo de pago
+                    var sumasPorTipoPago = from pedi in db.Pedido
+                                           where pedi.Id_TipoPago != null
+                                           group pedi by new { Fecha = DbFunctions.TruncateTime(pedi.Fecha.Value), pedi.Id_TipoPago } into pdp
+                                           select new
+                                           {
+                                               Suma = pdp.Sum(pedi => pedi.Importe_Total).ToString(),
+                                               Fecha = pdp.Key.Fecha,
+                                               Pedido = pdp
+                                           };
+
+                    var tiposPagos = from tipo in db.TipoPago
+                                     select tipo.Descripcion;
+
+                    //Verificar si no se encontraron pedidos
+                    if (sumasPorTipoPago.Any() && tiposPagos.Any())
+                    {
+                        chart1.Series.Clear();
+                        //Añadir tipos de pagos disponibles
+                        foreach(string i in tiposPagos)
+                        {
+                            chart1.Series.Add(i);
+                        }
+                        //Añadir al chart
+                        foreach (var i in sumasPorTipoPago)
+                        {
+                            string fecha = i.Fecha.Value.ToString("dd/MM/yyyy");
+
+                            try
+                            {
+                                int tipo = i.Pedido.FirstOrDefault().Id_TipoPago.Value - 1;
+                                chart1.Series[tipo].Points.AddXY(i.Fecha.Value.Date, double.Parse(i.Suma));
+                                chart1.Series[tipo].SmartLabelStyle.Enabled = true;
+                                chart1.Series[tipo].IsValueShownAsLabel = true;
+                                chart1.Series[tipo]["PixelPointWidth"] = "30";
+
+                            }
+                            catch (System.ArgumentException)
+                            {
+                                //Si ya existe, referenciarla
+                                chart1.Series[0].Points.AddXY(i.Fecha.Value.Date, double.Parse(i.Suma));
+                            }
+
+                        }
+
+                    }
+                    else
+                    {
+                        MessageBox.Show("No se encontraron pedidos", "Error", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("No se pudo traer los pedidos de la base de datos", "Error", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
         }
     }
 }
