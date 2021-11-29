5c4cfc8882cf6a026b8bdbe4c6e2e16070217497
martincou29 <martincoutinho00@gmail.com>
2021-11-29 09:53:57 -0300

    Grafico con periodo arreglado.

--- InfoBAR/Pedidos_Ventas/GraficoPorPeriodo.cs
+++ InfoBAR/Pedidos_Ventas/GraficoPorPeriodo.cs
@@ -25,7 +25,9 @@ namespace InfoBAR
 
         private void btnGrafico_Click(object sender, EventArgs e)
         {
-
+            Color[] colores = {Color.FromArgb(65, 140, 240), Color.FromArgb(252, 180, 65),
+                                 Color.FromArgb(224, 64, 10), Color.FromArgb(5, 100, 146) ,
+                                      Color.FromArgb(200, 140, 255),  Color.FromArgb(26, 59, 105)};
             using (InfobarEntities db = new InfobarEntities())
             {
                 //Traer todas las ventas/pedidos con tipo de pago y usuario
@@ -41,13 +43,19 @@ namespace InfoBAR
                 //Verificar si no se encontraron pedidos
                 if (pedidosYDetalles.Any())
                 {
+                    int f = 0;
                     chart1.Series.Clear();
+                    chart1.Series.Add("Ventas por periodo");
                     //Añadir al datagrid
                     foreach (var i in pedidosYDetalles)
                     {
                         string fecha = i.Fecha.Value.ToString("dd/MM/yyyy");
-                        chart1.Series.Add(fecha).Points.AddXY(i.Fecha.Value.Date, i.Suma);
-                        chart1.Series[fecha].IsValueShownAsLabel = true;
+                        chart1.Series[0].Points.AddXY(i.Fecha.Value.Date, i.Suma);
+                        chart1.Series[0].IsValueShownAsLabel = true;
+                        chart1.Series[0].Points[f].Color = colores[f];
+
+                        //Conteo
+                        f = f >= colores.Length - 1 ? 0 : f + 1;
                     }
 
                 }
